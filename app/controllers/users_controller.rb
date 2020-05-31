d36b1694224962ce43508252e7e7879fde33e3ee
sekishinya <sekishinya@example.com>
2020-05-31 11:27:32 +0900

    ログアウト機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
@@ -66,5 +67,12 @@ class UsersController < ApplicationController
       render("users/login_form")
     end
   end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice]  = "ログアウトしました"
+    redirect_to("/login")
+
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
     get "login" => "users#login_form"
     post "login" => "users#login"
+    post "logout" => "users#logout"
+
     post "users/:id/update" => "users#update"
     get "users/:id/edit" => "users#edit"
 
