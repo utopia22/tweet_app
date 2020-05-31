3b49aeadfb8ca5bdc29fdd16fbd8ba2489d7ff8b
sekishinya <sekishinya@example.com>
2020-05-31 16:44:08 +0900

    index投稿紐付け

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
-  
+
   def index
       @posts = Post.all.order(created_at: :desc)
   end
 
   def show
      @post = Post.find_by(id:params[:id])
+     @user = @post.user
   end
 
   def new
@@ -14,8 +15,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content])
-
+    @post = Post.new(
+      content: params[:content],
+      user_id: @current_user.id
+    )
     if @post.save
       flash[:notice] = "投稿を作成しました"
       redirect_to("/posts/index")
@@ -23,6 +26,7 @@ class PostsController < ApplicationController
       render("posts/new")
     end
   end
+
 
    def edit
      @post = Post.find_by(id: params[:id])
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
+  validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
 
-  validates :content, {presence: true}
-  validates :content, {length: {maximum: 140}}
+  def user
+    return User.find_by(id: self.user_id)
+  end
 
 end
