a6f38f56848fb3238c3067d344f761cab8bad978
sekishinya <sekishinya@example.com>
2020-05-31 10:28:37 +0900

    パスワードカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_114010) do
+ActiveRecord::Schema.define(version: 2020_05_31_011908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_05_29_114010) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
